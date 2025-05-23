f2bd0164570adcb2493069a6d5f09c640acec791
Tuan Dang <tuandang1210@gmail.com>
2025-05-23 15:34:24 +0700

    Cập nhật code

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Script for navigation bar
+const bar = document.getElementById('bar');
+const nav = document.querySelector('.nav');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    });
+}
+
+const close = document.getElementById('close');
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    });
+}
