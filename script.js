4698e2d60647c47b04e0ebf81adc49f9f375b1b2
Tuan Dang <tuandang1210@gmail.com>
2025-05-30 16:26:21 +0700

    update code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// header nav
 const bar = document.getElementById('bar');
 const nav = document.querySelector('.nav');
 
@@ -13,3 +14,36 @@ if (close) {
         nav.classList.remove('active');
     });
 }
+
+// newsletter
+  const button = document.getElementById("button-addon2");
+  const emailInput = document.getElementById("emailInput");
+
+  button.addEventListener("click", function () {
+    const email = emailInput.value.trim();
+
+    if (email.includes("@")) {
+      alert("Valid email! Registration successful.");
+    } else {
+      alert("Invalid email address. It must contain '@'.");
+    }
+  });
+
+// form-input contact
+function validateForm() {
+    const name = document.getElementById("nameInput").value.trim();
+    const email = document.getElementById("emailInput").value.trim();
+    const subject = document.getElementById("subjectInput").value.trim();
+    const message = document.getElementById("messageInput").value.trim();
+
+    if (!name || !email || !subject || !message) {
+      alert("Please fill in all fields.");
+      return;
+    }
+
+    if (!email.includes("@")) {
+      alert("Invalid email address. It must contain '@'.");
+      return;
+    }
+    alert("Message sent successfully!");
+  }
